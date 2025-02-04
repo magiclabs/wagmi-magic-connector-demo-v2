9957be5e850f2f922f4eda04eb360b92a3fdd572
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2025-02-04 10:02:02 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { createConfig, WagmiProvider } from "wagmi";
 import { http } from "viem";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { sepolia } from '@wagmi/core/chains'
+import { goerli, mainnet, polygonAmoy, sepolia } from "@wagmi/core/chains";
 import { dedicatedWalletConnector } from "@magiclabs/wagmi-connector";
 import Dashboard from "./components/Dashboard";
 
 const queryClient = new QueryClient();
 
-const config = createConfig({
-  chains: [sepolia],
+export const config = createConfig({
+  chains: [sepolia, mainnet, polygonAmoy, goerli],
   transports: {
-    [sepolia.id]: http(process.env.REACT_APP_RPC_URL)
+    [sepolia.id]: http(process.env.REACT_APP_SEPOLIA_RPC_URL),
+    [mainnet.id]: http(process.env.REACT_APP_ETHEREUM_RPC_URL),
+    [polygonAmoy.id]: http(process.env.REACT_APP_POLYGON_AMOY_RPC_URL),
   },
   autoConnect: true,
   connectors: [
     dedicatedWalletConnector({
-      chains: [sepolia],
+      chains: [sepolia, mainnet, polygonAmoy, goerli],
       options: {
+        networks: [
+          "sepolia",
+          "mainnet",
+          "goerli",
+          { chainId: 80002, rpcUrl: "https://polygon-amoy.drpc.org" },
+        ],
         apiKey: process.env.REACT_APP_MAGIC_API_KEY,
         isDarkMode: true,
         /* Make sure to enable OAuth options from magic dashboard */
@@ -24,10 +32,7 @@ const config = createConfig({
           providers: ["google", "twitter", "github"],
         },
         magicSdkConfiguration: {
-          network: {
-            rpcUrl: process.env.REACT_APP_RPC_URL,
-            chainId: 11155111,
-          },
+          network: "mainnet",
         },
       },
     }),
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,14 +1,47 @@
-import { useAccount, useEnsName, useDisconnect, useNetwork } from "wagmi";
+import { useAccount, useEnsName, useDisconnect, useSwitchChain } from "wagmi";
 import Balance from "./Balance";
 import SignMessage from "./SignMessage";
 import SendTransaction from "./SendTransaction";
 import StatusCircle from "./StatusCircle";
 import Divider from "./Divider";
+import { useState } from "react";
 
 const Wallet = () => {
-  const { address, connector: activeConnector, status, chain } = useAccount();
+  const {
+    address,
+    connector: activeConnector,
+    status,
+    chain,
+    chainId,
+  } = useAccount();
+  const {
+    switchChain,
+    error,
+    isPending,
+    status: switchStatus,
+    isSuccess,
+  } = useSwitchChain();
   const { data: ensName } = useEnsName({ address });
   const { disconnect } = useDisconnect();
+  const [selectedChain, setSelectedChain] = useState(chainId);
+
+  // console.log("switchStatus", switchStatus);
+  // console.log("isPending", isPending);
+  // console.log("isSuccess", isSuccess);
+  if (error) {
+    console.log("---error---", error);
+  }
+
+  const handleSwitchChain = () => {
+    console.log("Switching chain to", selectedChain);
+    switchChain({ chainId: selectedChain });
+  };
+
+  const handleChange = (event) => {
+    const chainId = Number(event.target.value);
+    setSelectedChain(chainId);
+    // switchChain({ chainId });
+  };
 
   return (
     <div className="wallet-container">
@@ -18,6 +51,19 @@ const Wallet = () => {
       </div>
 
       {chain && <div>Chain: {chain?.name}</div>}
+      <select>
+        <option value={1}>Mainnet</option>
+        <option value={11155111}>Sepolia</option>
+        <option value={80002}>Amoy</option>
+      </select>
+
+      <Divider />
+      <input
+        type="text"
+        value={selectedChain}
+        onChange={(e) => setSelectedChain(e.target.value)}
+      />
+      <button onClick={handleSwitchChain}>Switch Chain</button>
       <Divider />
       <div>Connected to {ensName ?? address}</div>
       <Balance address={address} />
